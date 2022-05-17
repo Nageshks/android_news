f2f803cbb007afb80c9d4fbfaae8251bf95d82cd
Nagesh <nageshempire@gmail.com>
2022-05-18 00:00:11 +0530

    Initial commit

---
+++ app/src/main/java/com/nageshempire/androidnews/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.nageshempire.androidnews.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
